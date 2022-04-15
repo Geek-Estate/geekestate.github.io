8f45a6472853ac85bb10c901ababef6d1ec44767
Jonathan Ursua <jonathan@caffeineinteractive.com>
2022-04-15 23:34:42 +0800

    fixed bug on accordion

--- js/main.js
+++ js/main.js
@@ -42,7 +42,6 @@ $(document).ready(function(){
   for (i = 0; i < acc.length; i++) {
       acc[i].addEventListener("click", function() {
           this.classList.toggle("active");
-          console.log("DDD");
           var accordionBody = this.nextElementSibling;
           if (accordionBody.style.maxHeight) {
               accordionBody.style.maxHeight = null;
